1babb0e495d9edcf6ba0eb4a39bca498d067c1af
Nick Bouldien <nick.bouldien@gmail.com>
2017-08-06 19:02:52 -0700

    added DetailViewController, can save todos

--- todo_list_swift/ViewController.swift
+++ todo_list_swift/ViewController.swift
@@ -8,18 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var table: UITableView!
 
-    var data: [String] = ["Row1", "Row2", "Row3"]
+    var data: [String] = []
+    var file: String!
+    var selectedRow:Int = -1
+    var newRowText:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.title = "Notes"
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
+        self.navigationItem.rightBarButtonItem = addButton
+        self.navigationItem.leftBarButtonItem = editButtonItem
         
+        let docsDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .allDomainsMask, true)
+        file = docsDir[0].appending("notes.txt")
+        
+        load()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if selectedRow == -1 {
+            return
+        }
+        data[selectedRow] = newRowText
+        if newRowText == "" {
+            data.remove(at: selectedRow)
+        }
+        table.reloadData()
+        save()
+    }
+    
+    func addNote() {
+        if table.isEditing {
+            return
+        }
+        let name: String = "Row \(data.count + 1)"
+        data.insert(name, at: 0)
+        let indexPath: IndexPath = IndexPath(row: 0, section: 0)
+        table.insertRows(at: [indexPath], with: .fade)
+        table.selectRow(at: indexPath, animated: true, scrollPosition: .none)
+        performSegue(withIdentifier: "detail", sender: nil)
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        table.setEditing(editing, animated: animated)
+        save()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let detailView:DetailViewController = segue.destination as! DetailViewController
+        let selected = table.indexPathForSelectedRow!.row
+        detailView.masterView = self
+        detailView.setText(t: data[selected])
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        data.remove(at: indexPath.row)
+        table.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(data[indexPath.row])
+        performSegue(withIdentifier: "detail", sender: nil)
+    }
+    
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,6 +92,26 @@ class ViewController: UIViewController, UITableViewDataSource {
         return cell
     }
     
+    func save() {
+//        UserDefaults.standard.set(data, forKey: "notes")
+//        UserDefaults.standard.synchronize()
+        
+        let newData: NSArray = NSArray(array: data)
+        newData.write(toFile: file, atomically: true)
+    }
+    
+    func load() {
+//        if let loadedData = UserDefaults.standard.value(forKey: "notes") as? [String] {
+//            data = loadedData
+//            table.reloadData()
+//        }
+//        
+        if let loadedData = NSArray(contentsOfFile:file) as? [String] {
+            data = loadedData
+            table.reloadData()
+        }
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
